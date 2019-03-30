8bf894b542a230ed07c6e1d2e8c374e9778288c5
unknown <tiago.recatia@hotmail.com>
2019-03-30 17:31:19 +0000

    Packages

--- TrabalhoPA/src/de/TrabalhoPA.java
+++ TrabalhoPA/src/de/TrabalhoPA.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabalhopa;
+package de;
 
 /**
  *
@@ -15,7 +15,7 @@ public class TrabalhoPA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
