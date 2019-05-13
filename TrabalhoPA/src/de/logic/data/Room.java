38ad26b9ff1cabd1e91ad213e9f6eb2893a1ffc3
Ricardo <R_P_1997@hotmail.com>
2019-05-13 23:29:13 +0100

    MoveAliens (Wide Search) Complete
    * Remove SystemOuts after

--- TrabalhoPA/src/de/logic/data/Room.java
+++ TrabalhoPA/src/de/logic/data/Room.java
@@ -4,7 +4,9 @@ import de.logic.data.members.CrewMember;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Room implements Serializable, Constants{
     private DataGame game;
@@ -139,111 +141,60 @@ public class Room implements Serializable, Constants{
         trapInside = null;
     }
     
-    /*public Room _chooseClosestRoom_Priority(List <Room> roomsSeen, List <Room> path, int pathSize){
-        //Phase 1 - Check if current room is what we are looking for
-        //Phase 2 - Check if any of the closest rooms are what we are looking for
-        //Phase 3 - Check if the closest rooms of the closest rooms are what we are looking for
-        //Phase 4 - Repeat phase 2 and 3 for every closest room
-        
-        //If already searched for the NUM_ROOMS existing rooms (to prevent infinite loop)
-        if(pathSize >= NUM_ROOMS)
-            return null;
-        
-        //Phase 1 - Only supposed to be true on first room (If the current room already has a crew member)
-        if(!this.getMembersInside().isEmpty()){
-            return this;
-        }
-        
-        pathSize++;
-        
-        //Phase 2
-        for(Room room:closestRooms){
-            //Check if current (closest) room has already been checked
-            if(roomsSeen.get(room.getId()) != null)
-                continue;
-
-            roomsSeen.add(room.getId(), room);
-
-            //If there are crew members inside and the room is NOT sealed
-            if(!room.getMembersInside().isEmpty() && !room.getIsSealed()){
-                path.add(pathSize, room);
-                return room;
-            }
-        }
-
-        //Phase 3
-        for(Room r:closestRooms){
-            for(Room room:r.getClosestRooms()){
-                //Check if current room has already been checked
-                if(roomsSeen.get(room.getId()) != null)
-                    continue;
-
-                roomsSeen.add(room.getId(), room);
-
-                //If there are crew members inside and the room is NOT sealed
-                if(!room.getMembersInside().isEmpty() && !room.getIsSealed()){
-
-                    return room;
-                }else if(pathSize > 1){
-                    return null;
-                }
-            }
-        }
-
-        //Phase 4
-        for(Room room:closestRooms){
-            if(roomsSeen.get(room.getId()) != null)
-                room._chooseClosestRoom_Priority(roomsSeen, path, pathSize);
-        }
-
-        return null;
-    }*/
-    
     public Room chooseClosestRoom(List <Room> nextClosestRooms){
-        HashMap<Integer, Room> roomsSeen = new HashMap<>();
         
-        if(this.getMembersInside().isEmpty())
-            return chooseClosestRoom_Algorithm(nextClosestRooms, roomsSeen, 0);
+        Room room = chooseClosestRoom_Algorithm();
+        if(room != null)
+            System.out.println("Quarto com " + room.getMembersInside().size() + " membros");
+        else
+            System.out.println("Não encontrado");
         
-        game.addLog("Alien already inside a room with a crew member!");
-        return null;
+        return room;
     }
     
-    public Room chooseClosestRoom_Algorithm(List <Room> nextClosestRooms, HashMap<Integer, Room> roomsSeen, int pathSize){
-        if(++pathSize > 12)
+    public Room chooseClosestRoom_Algorithm(){
+        Queue<Room> toBeSeen = new LinkedList();
+        List<Room> alreadySeen = new ArrayList();
+        
+        Room auxRoom = null;
+        toBeSeen.add(this);
+        
+        while(alreadySeen.size() < NUM_ROOMS){
+            System.out.println("alreadySeen size: " + alreadySeen.size());
+            
+            auxRoom = toBeSeen.poll();
+            alreadySeen.add(auxRoom);
+            
+            System.out.println("Quarto a ser visto: " + auxRoom.getName());
+            
+            if(!auxRoom.getMembersInside().isEmpty() && !auxRoom.getIsSealed())
+                break;
+                
+            for(Room neighbor:auxRoom.getClosestRooms()){
+                if(!alreadySeen.contains(neighbor)){
+                    toBeSeen.add(neighbor);
+                }
+            }
+        }
+        
+        if(alreadySeen.size() == NUM_ROOMS)
             return null;
         
-        System.out.println("pathSize increased: " + pathSize);
-        
-        for(Room room:nextClosestRooms){
-            if(roomsSeen.get(room.getId()) != null){
-                System.out.println("room: " + room.getId() + " already inside");
-                continue;
-            }
-            
-            System.out.println("roomsSeen added: " + room.getId());
-            roomsSeen.put(room.getId(), room);
-
-            if(!room.getMembersInside().isEmpty() && !room.getIsSealed()){
-                System.out.println("room found: " + room.getId());
-                return room;
-            }
-        }
-        
-        List <Room> aux_closestRooms = new ArrayList<>();
-        for(Room room:nextClosestRooms){
-            for(Room _room:room.getClosestRooms()){
-                if(roomsSeen.get(_room.getId()) != null){
-                    System.out.println("room: " + _room.getId() + " already inside");
-                    continue;
+        while(!alreadySeen.isEmpty()){
+            for(int i = 0; i < alreadySeen.size(); i++){
+                if(auxRoom.getClosestRooms().contains(alreadySeen.get(i))){
+                    if(alreadySeen.get(i) == this){
+                        System.out.println("Quarto é vizinho do quarto atual... A retornar o quarto: " + auxRoom.getName() );
+                        return auxRoom;
+                    }
+                    System.out.println("Vizinho de: " + auxRoom.getName() + " encontrado: " + alreadySeen.get(i).getName());
+                    auxRoom = alreadySeen.get(i);
+                    break;
                 }
-                aux_closestRooms.add(_room);
             }
         }
         
-        System.out.println("next phase: " + pathSize + " auxClosestRooms size: " + aux_closestRooms.size());
-        
-        return chooseClosestRoom_Algorithm(aux_closestRooms, roomsSeen, pathSize);
+        return null;
     }
 
     @Override
